79689ba7fa36359dff976e21712208a9e7f2db49
happy-spark <0117spark0117@gmail.com>
2022-10-23 18:26:19 +0900

    Sin is complete

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function joinWeb() {
 }
 
 joinWeb()
+
+$("#sin-input").on("propertychange change paste input", function() {
+    var sinX = document.getElementById("sin-input").value;
+    document.getElementById("sin-result").innerHTML = '<p style="font-size: 35px">=' + Math.sin(sinX * Math.PI / 180).toFixed(8); + '</p>';
+});
